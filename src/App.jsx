d5e775b79870be02f6d4fb907ac8ff3cece38b24
Maik125 <miguelfin43@gmail.com>
2023-03-28 22:30:19 -0600

    Se creo la page Home
    Faltan estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,59 @@
-import { useState } from 'react'
-import CrearTickets from './assets/pages/CrearTickets';
-import Tickets from './assets/pages/Tickets';
-import Tablero from './assets/pages/Tablero';
-import MisTickets from './assets/pages/MisTickets';
-import { Route, Routes } from 'react-router-dom';
-import './App.scss'
-import Layout from './assets/pages/Layout';
+
+
 import Home from './assets/pages/Home';
 
+import { useState } from "react";
+import CrearTickets from "./assets/pages/CrearTickets";
+import Tickets from "./assets/pages/Tickets";
+import Tablero from "./assets/pages/Tablero";
+import MisTickets from "./assets/pages/MisTickets";
+import { Route, Routes } from "react-router-dom";
+import "./App.scss";
+import Layout from "./assets/pages/Layout";
+import Navbar from "./componentes/navbar/NavBar.jsx";
+import Footer from "./componentes/footer/Footer";
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
+
     <div >
       
-      <Routes>
-        <Route path='/' element={<Layout/>}>
-        <Route path='/CrearTickets' element={<CrearTickets/>}> </Route>
-        <Route path='/Tickets' element={<Tickets/>}> </Route>
-        <Route path='/MisTickets' element={<MisTickets/>}> </Route>
-        <Route path='/Home' element={<Home/>}> </Route>
-        <Route path='/' element={<Tablero/>}> </Route>
-        <Route path='*' element={<Tablero/>}> </Route>
-        </Route>
-      </Routes>
-    </div>
-    
-    </div>
-  )
-}
 
-export default App
+      <div>
+        
+            <Navbar />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/CrearTickets" element={<CrearTickets />}>
+              {" "}
+            </Route>
+            <Route path="/Tickets" element={<Tickets />}>
+              {" "}
+            </Route>
+            <Route path="/MisTickets" element={<MisTickets />}>
+              {" "}
+            </Route>
+            <Route path="/Home" element={<Home />}>
+              {" "}
+            </Route>
+            <Route path="/" element={<Tablero />}>
+              {" "}
+            </Route>
+            <Route path="*" element={<Tablero />}>
+              {" "}
+            </Route>
+          </Route>
+        </Routes>
+            <Footer/>
+      </div>
+
+      </div>
+  
+
+
+</div>)}
+export default App;
--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -7,16 +7,28 @@ function Home() {
         <div>
             
             <section className="wlcm">
-
+                    <h1>!Bienvenido¡</h1>
             </section>
             <section className="Abu">
-
+                <div className="container1">
+                    <div className="img"></div>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident iure libero in ipsum veniam, unde labore temporibus molestias, repellat alias laudantium excepturi sapiente nihil voluptatibus soluta. Mollitia et iste a.</p>
+                </div>
+                <div className="container2">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsa itaque quis dolorum veritatis magnam amet incidunt, odio dicta doloribus qui consequuntur deserunt illum vel commodi veniam alias cum. At.</p>
+                <div className="img"></div>
+                </div>
             </section>
             <section className="tck">
-
+            <span id="warn" class="material-symbols-outlined">
+warning
+</span>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nisi sequi accusamus, accusantium obcaecati et, officiis sunt ab odit qui laborum eaque pariatur voluptatibus dolor iusto cupiditate? Pariatur, reprehenderit expedita!</p>
             </section>
             <section className="cont">
-
+            <span id="cont" class="material-symbols-outlined">
+perm_phone_msg
+</span>
             </section>
         </div>
     </div>
